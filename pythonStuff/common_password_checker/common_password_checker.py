b301ac84197df151e46e419b9ea2f8fb19e37ef7
Alen Medjimorec <alen.medjimorec@gmail.com>
2023-11-09 17:15:06 +0100

    Common Password Checker finished

---
+++ pythonStuff/common_password_checker/common_password_checker.py
@@ -0,0 +1,17 @@
+def check_password(password: str):
+    with open("passwords.txt", "r") as file:
+        common_passwords: list[str] = file.read().splitlines()
+        
+    for i, common_password in enumerate(common_passwords, start=1):
+        if password == common_password:
+            print(f"{password}: COMMON! (#{i})")
+            return
+    
+    print(f"{password}: UNIQUE!")
+
+def main():
+    user_password: str = input("Enter a password: ")
+    check_password(user_password)
+
+if __name__ == "__main__":
+    main()
